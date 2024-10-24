97a2d69c8f09c029635c36f5660e86f6a3e0efae
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-10-23 21:54:32 -0300

    a

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,6 +10,12 @@ const Home = () => {
                 <h1>Lançamento em Janeiro de 2025</h1>
                 <h2>Junte-se ao melhor capitulo desse lado do Warp!</h2>
             </div>
+            <div className="anuncio-subtitle-container">
+              <h3>Inscreva-se para receber atualizações mais recentes par<br />o primeiro tabletop baseado em warhammer40k do Brasil.</h3>
+              <form>
+                <label className='anuncio-subtitle-email-label' htmlFor="email-home"><input type="email" name="email-home" id="email-home" placeholder='Email' /><button className='anuncio-subtitle-email-button'>Inscrição</button></label>
+              </form>
+            </div>
         </div>
     </div>
   )
---
+++ src/components/utils/Sociais.tsx
@@ -0,0 +1,14 @@
+import { InstagramLogo } from '@phosphor-icons/react';
+import React from 'react'
+
+const Sociais = () => {
+  return (
+    <ul>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer"><InstagramLogo /></a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer"><InstagramLogo /></a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer"><InstagramLogo /></a></li>
+    </ul>
+  )
+}
+
+export default Sociais;
